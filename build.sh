b0eac77c78d2fce55974cee692d471d9846d6a43
Guilhem Lavaux <guilhem.lavaux@iap.fr>
2025-09-02 21:19:41 +0200

    Make 0.2.0

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 set -e
-VER=0.1.1
+VER=0.2.0
 podman build -t ollama-proxy:$VER --build-arg VER=$VER .
 podman push ollama-proxy:$VER docker.io/glvx/ollama-proxy:$VER
 podman push ollama-proxy:$VER docker.io/glvx/ollama-proxy:latest
